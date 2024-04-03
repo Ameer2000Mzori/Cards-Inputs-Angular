aade0d6da36b354eb5787dcf64d404791f0711d2
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-04-03 13:21:25 +0200

    generated new coard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardsComponent } from './cards/cards.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Cards-Inputs-Angular';
